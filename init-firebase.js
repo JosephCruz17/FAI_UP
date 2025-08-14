677f80bb848866f58b0c5baddfe27882368ef866
JosephCruz17 <Josephrafaelcruz17@gmail.com>
2025-08-14 15:36:12 -0400

    Add files via upload

---
+++ init-firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDAqQA5dFsCjvlM-U4o9DbTP4ieKcQ5D8Y",
+  authDomain: "fire-chat-11a2c.firebaseapp.com",
+  databaseURL: "https://fire-chat-11a2c-default-rtdb.firebaseio.com",
+  projectId: "fire-chat-11a2c",
+  storageBucket: "fire-chat-11a2c.firebasestorage.app",
+  messagingSenderId: "78767923732",
+  appId: "1:78767923732:web:d595f151b6238f7dc6fd01",
+  measurementId: "G-XL0FYNSZ7M"
+};
+
+// initialize the firebase app with this built-in function
+firebase.initializeApp(firebaseConfig);
